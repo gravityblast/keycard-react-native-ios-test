f7e902ce627899ab06c2d9e49f31082d3fa9c99a
Andrea Franz <andrea@gravityblast.com>
2020-12-01 21:50:42 +0100

    sign with cash applet

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 import { NativeModules } from 'react-native'
+import { RNStatusKeycard } from 'react-native-status-keycard';
 
 NativeModules.RNStatusKeycard.nfcIsSupported().then((b) => console.log("Has NFC: " + b));
 
 export default function App() {
+  const clickHandler = () => {
+    hash = "0x0000000000000000000000000000000000000000000000000000000000000001";
+    // NativeModules.RNStatusKeycard.nfcIsSupported()
+    NativeModules.RNStatusKeycard.signPinless(hash)
+      .then(x => Alert.alert(x.toString()))
+      .catch(x => Alert.alert(x.toString()));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test</Text>
+      <Button
+        title="SIGN"
+        onPress={clickHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
