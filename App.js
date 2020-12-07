3bdf25d2afd9b2a884c3dcae50d02847d164b9a3
Andrea Franz <andrea@gravityblast.com>
2020-12-07 20:39:46 +0100

    WIP

--- App.js
+++ App.js
@@ -1,27 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { EventRegister } from 'react-native-event-listeners';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, Button, Alert,  } from 'react-native';
 
-import { NativeModules } from 'react-native'
+import { NativeModules, DeviceEventEmitter, NativeEventEmitter, Platform } from 'react-native'
 import { RNStatusKeycard } from 'react-native-status-keycard';
 
 NativeModules.RNStatusKeycard.nfcIsSupported().then((b) => console.log("Has NFC: " + b));
 
 export default function App() {
-  const clickHandler = () => {
+  const startHandler = () => {
     hash = "0x0000000000000000000000000000000000000000000000000000000000000001";
     // NativeModules.RNStatusKeycard.nfcIsSupported()
-    NativeModules.RNStatusKeycard.signPinless(hash)
-      .then(x => Alert.alert(x.toString()))
+    // NativeModules.RNStatusKeycard.signPinless(hash)
+    //   .then(x => Alert.alert(x.toString()))
+    //   .catch(x => Alert.alert(x.toString()));
+    NativeModules.RNStatusKeycard.startNFC()
+      .then(x => console.log("-------------- started"))
       .catch(x => Alert.alert(x.toString()));
   }
+
+  const stopHandler = () => {
+    hash = "0x0000000000000000000000000000000000000000000000000000000000000001";
+    // NativeModules.RNStatusKeycard.nfcIsSupported()
+    // NativeModules.RNStatusKeycard.signPinless(hash)
+    //   .then(x => Alert.alert(x.toString()))
+    //   .catch(x => Alert.alert(x.toString()));
+    NativeModules.RNStatusKeycard.stopNFC()
+      .then(x => console.log("-------------- stopped"))
+      .catch(x => Alert.alert(x.toString()));
+  }
+
+  useEffect(() => {
+    console.log("------------------OKOKOK")
+    // const emitter = new NativeEventEmitter(NativeModules.ReactNativeEventEmitter);
+
+    // console.log("====================== OK")
+    // EventRegister.addEventListener('keyCardOnConnected', (data) => {
+    //   Alert.alert(`ON CONNECT: ${data}`);
+    // })
+
+    // EventRegister.addEventListener('keyCardOnDisconnected', (data) => {
+    //   Alert.alert(`ON DISCONNECT: ${data}`);
+    // })
+  });
 
   return (
     <View style={styles.container}>
       <Text>Test</Text>
       <Button
-        title="SIGN"
-        onPress={clickHandler}
+        title="START"
+        onPress={startHandler}
+      />
+      <Button
+        title="STOP"
+        onPress={stopHandler}
       />
       <StatusBar style="auto" />
     </View>
